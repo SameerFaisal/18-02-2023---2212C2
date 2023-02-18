ac7a3946946d96b4e206986eae65b983b2a874c0
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-18 16:49:14 +0500

    18-02-2023

---
+++ 18-02-2023 (2212C2)/app.js
@@ -0,0 +1,64 @@
+// document.write("This is batch 2212C2")
+// alert("This is batch 2212C2")
+// console.log("This is batch 2212C2")
+
+// let stdName="John"
+
+// console.log(stdName)
+
+// var std=null
+
+// alert(std)
+
+// document.write("This is 1")
+// document.write("<br>")
+// document.write("This is 2")
+
+// document.write("<h1>This is a heading</h1>")
+
+
+// let firstNumber=prompt("Enter your first number") 
+// let secondNumber=prompt("Enter second Number")  
+// let result=firstNumber+secondNumber
+// let result=firstNumber-secondNumber
+// let result=firstNumber*secondNumber
+// let result=firstNumber/secondNumber
+// let result=firstNumber%secondNumber
+// let result=firstNumber+secondNumber
+// document.write(result) 
+// let stdRoll
+
+// let firstNumber=prompt("Enter your first number") 
+// let secondNumber=+prompt("Enter second Number") 
+// let thirdNumber=+prompt("Enter third Number") 
+// let fourthNumber=prompt("Enter your fourth number") 
+
+
+// let result=firstNumber-secondNumber/thirdNumber+fourthNumber
+// document.write(result) 
+
+// let num1=+prompt("Enter number 1")
+// let num2=+prompt("Enter number 2")
+
+// let add=num1+num2
+// let sub=num1-num2
+// let mul=num1*num2
+// let div=num1/num2
+
+// document.write(add)
+// document.write("<br>")
+// document.write(sub)
+// document.write("<br>")
+// document.write(mul)
+// document.write("<br>")
+// document.write(div)
+
+// let exp=2+(3/2)*6+5*(3*5)
+// alert(exp)
+
+
+
+
+
+
+
